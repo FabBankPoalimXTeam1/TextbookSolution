ad09c47ce4155da61cdc732c890c24e11aa6a3c0
doron segev <om205@bhpintune.onmicrosoft.com>
2018-11-14 12:06:55 +0200

    update 11

--- webapp/js/cognito-auth.js
+++ webapp/js/cognito-auth.js
@@ -152,7 +152,7 @@ var BankApp = window.BankApp || {};
             headers: {
                 Authorization: authToken
             },
-            data: jQuery.param({ dstusername: dstusername, sum : sum}) ,
+            data:{ dstusername: dstusername, sum: sum} ,
             contentType: 'application/json',
             success: successTransferToAccount,
             error: function ajaxError(jqXHR, textStatus, errorThrown) {
