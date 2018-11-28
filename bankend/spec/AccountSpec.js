f9fbc1a6f5397e215d8ac57e9cb3d68893b03822
David Halimi <davidmagali@gmail.com>
2018-11-28 10:22:18 +0200

    remove the test

--- bankend/spec/AccountSpec.js
+++ bankend/spec/AccountSpec.js
@@ -20,8 +20,8 @@ describe("Account", function() {
 	expect (balance).toEqual(200);
     });
 
-    it("return the right error code when no balance at the customer account", async function() {
-        
+    xit("return the right error code when no balance at the customer account", async function() {
+
         var srcUserName = "test_balance_src-at-kashyoo.com";
         var dstUserName = "TEST2-at-kashyoo.com";
 
